3dad96fe4f6f293f9e14f1101018d7d237f9752c
Tobi Schäfer <tobi.schaefer@gmail.com>
2015-02-04 18:53:29 +0100

    Adaptation to phpBB 3.1.3

--- event/listener.php
+++ event/listener.php
@@ -28,9 +28,9 @@ class listener implements EventSubscriberInterface
 	/** @var \phpbb\request\request */
 	protected $request;
 	
-	/** @var $login_try */
-	private $login_try = false;
-
+	/** @var \phpbb\log\log */
+	protected $log;
+	
 	/**
 	* Constructor
 	*
@@ -40,12 +40,13 @@ class listener implements EventSubscriberInterface
 	* @param \phpbb\request\request		$request
 	* @access public
 	*/
-	public function __construct(\phpbb\template\template $template, \phpbb\user $user, \phpbb\db\driver\driver_interface $db, \phpbb\request\request $request)
+	public function __construct(\phpbb\template\template $template, \phpbb\user $user, \phpbb\db\driver\driver_interface $db, \phpbb\request\request $request, \phpbb\log\log $log)
 	{
 		$this->template = $template;
 		$this->user = $user;
 		$this->db = $db;
 		$this->request = $request;
+		$this->log = $log;
 	}
 
 	/**
@@ -59,14 +60,13 @@ class listener implements EventSubscriberInterface
 	{
 		return array(
 			'core.user_setup'			=> 'user_setup',
-			'core.login_box_redirect'	=> 'login_box_redirect',
-			'core.page_footer'			=> 'page_footer',
+			'core.login_box_failed'		=> 'login_box_failed',
 			'core.page_header'			=> 'page_header',
 		);
 	}
 
 	/**
-	* Event on every login try to set $this->login_try on true
+	* Add language on user setup
 	*
 	* @param object $event The event object
 	* @return null
@@ -75,49 +75,25 @@ class listener implements EventSubscriberInterface
 	public function user_setup($event)
 	{	
 		$this->user->add_lang_ext('tas2580/failedlogins', 'common');
-		if($this->request->is_set('login'))
-		{
-			$this->login_try = true;
-		}
 	}
 	
 	/**
-	* If login success set $this->login_try on false
+	* If login failed set the conter +1
 	*
 	* @param object $event The event object
 	* @return null
 	* @access public
 	*/
-	public function login_box_redirect($event)
+	public function login_box_failed($event)
 	{
-		$this->login_try = false;
-	}
+		$sql = 'UPDATE ' . USERS_TABLE . ' SET failed_logins_count = failed_logins_count + 1
+					WHERE username = "' . $this->db->sql_escape($event['username']) . '"';
+		$this->db->sql_query($sql);
 
-	/**
-	* If $this->login_try is stil true the login failed
-	*
-	* @param object $event The event object
-	* @return null
-	* @access public
-	*/
-	public function page_footer($event)
-	{
-		if($this->login_try === true)
-		{
-			global $phpbb_log;
-			$username = $this->request->variable('username', '', true);
-
-			$sql = 'UPDATE ' . USERS_TABLE . ' SET failed_logins_count = failed_logins_count + 1
-						WHERE username = "' . $this->db->sql_escape($username) . '"';
-			$this->db->sql_query($sql);
-			
-			$this->user->add_lang_ext('tas2580/failedlogins', 'common');
-			$user_ip = (empty($this->user->ip)) ? '' : $this->user->ip;
-			$phpbb_log->add('user', ANONYMOUS, $user_ip, 'TRY_TO_LOGIN_FAIL', time(), array(
-				'reportee_id'   => ANONYMOUS,
-				'username'      => $username,
-			 ));
-		}
+		$this->log->add('user', ANONYMOUS, $this->user->ip, 'TRY_TO_LOGIN_FAIL', time(), array(
+			'reportee_id'   => ANONYMOUS,
+			'username'      => $event['username'],
+		 ));
 	}
 	
 	/**
