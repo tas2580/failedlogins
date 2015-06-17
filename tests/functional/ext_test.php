3efaacb17001a2803526c4707aeefc4e4cadc5d9
tas2580 <tobi.schaefer@gmail.com>
2015-06-17 15:07:51 +0200

    Added tests

--- tests/functional/ext_test.php
+++ tests/functional/ext_test.php
@@ -16,4 +16,9 @@ class ext_test extends \phpbb_functional_test_case
 	{
 		return array('tas2580/failedlogins');
 	}
+	
+	public function test_language()
+	{
+		$this->add_lang_ext('tas2580/failedlogins', 'common');
+	}
 }
